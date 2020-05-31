82975f3e3a9d9e109e9370036c96f8acaddd9809
Drogebot <Drogebot@hotmail.com>
2020-05-31 20:34:13 +0200

    Added ToS page and styling

--- src/pages/Terms.js
+++ src/pages/Terms.js
@@ -1,13 +1,36 @@
 import React from 'react'
 
 function Terms() {
-  return (
-    <div className="secondaryWrapper">
-
-      <h1 style={{color: "white", margin: "10px 0px 0px 20px"}}>Terms of service page</h1>
-      
-    </div>
-  )
+	return (
+		<div className="secondaryWrapper center">
+		
+			<div className="ToS">
+			
+				<h1><span style={{color: "#E7AA0F"}}>Terms</span> of Service</h1>
+				
+				<div className="ToS-body">
+					
+					<h2>LOREM IPSUM</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					
+					<h2>LOREM IPSUM</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					
+					<h2>LOREM IPSUM</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+					
+					<h2>LOREM IPSUM</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+					
+					<h2>LOREM IPSUM</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+	)
 }
 
 export default Terms;
